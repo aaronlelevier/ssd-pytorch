f734edddb86333a73530916de1742b0c69583496
aaronlelevier <aaron.lelevier@gmail.com>
2018-10-25 15:59:56 -0700

    CustomHead returning a 64 vector for bbs and 320 vector for cats, which acts as one-hot encoded

--- ssdmultibox/models.py
+++ ssdmultibox/models.py
@@ -7,32 +7,60 @@ from torchvision import models
 from ssdmultibox.datasets import TrainPascalDataset
 
 
+class LinearCats(nn.Module):
+    """
+    Takes a conv of [4, 20, 4, 4] and returns a vector of 320
+    """
+    def __init__(self):
+        super().__init__()
+        self.conv2d_3 = nn.Conv2d(20, 40, kernel_size=3, padding=1)
+        self.conv2d_4 = nn.Conv2d(40, 80, 3, padding=1)
+
+    def forward(self, x):
+        x = F.relu(self.conv2d_3(x)) # [4, 40, 4, 4]
+        x = self.conv2d_4(x) # [4, 80, 4, 4]
+        x = F.relu(F.max_pool2d(x, 2)) # [4, 80, 2, 2]
+        return x.view(-1, 320) # [4, 320]
+
+
+class LinearBbs(nn.Module):
+    """
+    Takes a conv of [4, 20, 4, 4] and returns a vector of 64
+    """
+    def __init__(self):
+        super().__init__()
+        self.conv2d_3 = nn.Conv2d(20, 40, kernel_size=3, padding=1)
+        self.linear128 = nn.Linear(160, 128)
+        self.linear64 = nn.Linear(128, 64)
+
+    def forward(self, x):
+        out9 = self.conv2d_3(x) # [4, 40, 4, 4]
+        out9_mp = F.relu(F.max_pool2d(out9, kernel_size=2)) # [4, 40, 2, 2]
+        flat = out9_mp.view(4, -1) # [4, 160]
+        flat128 = F.relu(self.linear128(flat))
+        flat64 = F.relu(self.linear64(flat128))
+        return flat64
+
+
 class CustomHead(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv2d_1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2d_2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2d_drop = nn.Dropout2d()
+        self.conv2d_3 = nn.Conv2d(20, 40, kernel_size=3, padding=1)
         self.linear = nn.Linear(512, 784)
-        self.linear_cats = nn.Linear(320, 16*20)
-        self.linear_bbs = nn.Linear(320, 16*4)
+        self.linear_cats = LinearCats()
+        self.linear_bbs = LinearBbs()
 
     def forward(self, outputs):
-        """
-        Reeturns a list of: [torch.Size([4, 64]), torch.Size([4, 320])]
-
-        TODO: labels will have to be flattened. Currently they're [16, 4] and [16, 20]
-        """
         out3 = self.linear(outputs)
         out4 = torch.reshape(out3, (4, 28, 28))
-        out7 = self.conv2d_1(out4.unsqueeze(1)) # [4, 10, 24, 24]
+        out5 = out4.unsqueeze(1) # [4, 1, 28, 28]
+        out7 = self.conv2d_1(out5) # [4, 10, 24, 24]
         out7_mp = F.relu(F.max_pool2d(out7, kernel_size=2)) # [4, 10, 12, 12]
-        out8_mp = F.relu(F.max_pool2d(self.conv2d_2(out7_mp), kernel_size=2)) # [4, 20, 4, 4]
-        flat = out8_mp.view(4, 320) # [4, 320]
-
-        cats_pred = self.linear_cats(flat)
-        bbs_pred = self.linear_bbs(flat)
-        return [bbs_pred, cats_pred]
+        out8 = self.conv2d_2(out7_mp) # [4, 20, 8, 8]
+        out8_mp = F.relu(F.max_pool2d(out8, kernel_size=2)) # [4, 20, 4, 4]
+        return self.linear_bbs(out8_mp), self.linear_cats(out8_mp)
     
 
 def train():
@@ -55,6 +83,9 @@ def train():
     optimizer.zero_grad()
 
     # forward pass
-    outputs = model(ims)
+    pred_bbs, pred_cats = model(ims)
+
+    # category loss
+    cats_bce = F.binary_cross_entropy_with_logits(pred_cats, cats)
 
     return outputs
