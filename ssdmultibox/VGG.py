9dd0d259c64ae282ae79a71797479cf2a53bddbf
aaronlelevier <aaron.lelevier@gmail.com>
2018-11-03 08:13:32 -0700

    Combined model with VGGBase, BlocksMiddle, and OutConvHead

--- ssdmultibox/VGG.py
+++ ssdmultibox/VGG.py
@@ -70,7 +70,6 @@ out8_2 = F.relu(conv8_2(out8))
 blocks['block8'] = out8_2
 out8_2.shape
 
-
 # block9
 conv9 = nn.Conv2d(512, 128, kernel_size=1, padding=1)
 out9 = F.relu(conv9(out8_2))
@@ -95,7 +94,49 @@ out11 = F.relu(conv11(out10_2))
 conv11_2 = nn.Conv2d(128, 256, kernel_size=3)
 out11_2 = F.relu(conv11_2(out11))
 blocks['block11'] = out11_2
-out11_2.shape
+
+
+
+class BlocksCustomHead(nn.Module):
+    "Returns the blocks for the OutCustomHead"
+    def __init__(self):
+        super().__init__()
+        self.conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
+        self.conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
+        self.conv8 = nn.Conv2d(1024, 256, kernel_size=1, padding=1)
+        self.conv8_2 = nn.Conv2d(256, 512, kernel_size=3, stride=2)
+        self.conv9 = nn.Conv2d(512, 128, kernel_size=1, padding=1)
+        self.conv9_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2)
+        self.conv10 = nn.Conv2d(256, 128, kernel_size=1, padding=1)
+        self.conv10_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2)
+        self.conv11 = nn.Conv2d(256, 128, kernel_size=1)
+        self.conv11_2 = nn.Conv2d(128, 256, kernel_size=3)
+
+    def forward(self, x_and_blocks):
+        x, blocks = x_and_blocks
+        # block6
+        out6 = F.dropout2d(F.relu(self.conv6(x)))
+        # block7
+        out7 = F.dropout2d(F.relu(self.conv7(out6)))
+        blocks['block7'] = out7
+        # block8
+        out8 = F.relu(self.conv8(out7))
+        out8_2 = F.relu(self.conv8_2(out8))
+        blocks['block8'] = out8_2
+        # block9
+        out9 = F.relu(self.conv9(out8_2))
+        out9_2 = F.relu(self.conv9_2(out9))
+        blocks['block9'] = out9_2
+        # block10
+        out10 = F.relu(self.conv10(out9_2))
+        out10_2 = F.relu(self.conv10_2(out10))
+        blocks['block10'] = out10_2
+        # block11
+        out11 = F.relu(self.conv11(out10_2))
+        out11_2 = F.relu(self.conv11_2(out11))
+        blocks['block11'] = out11_2
+        return blocks
+
 
 # check block shapes
 for k,v in blocks.items():
