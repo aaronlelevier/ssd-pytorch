322ab1d14a2216e4212d2d7bc916178466f6a489
aaronlelevier <aaron.lelevier@gmail.com>
2018-11-19 06:44:31 -0800

    improve v2.TrainPascalDataset docstring

--- ssdmultibox/datasets/v2.py
+++ ssdmultibox/datasets/v2.py
@@ -9,6 +9,18 @@ CATS = 'cats'
 class TrainPascalDataset(TrainPascalDataset):
 
     def __getitem__(self, idx):
+        """
+        Returns a single item from the dataset
+
+        Args:
+            idx (int)
+        Returns:
+            image_id (int)
+            chw_im (3d array): CHW formatted image 3d array
+            gt_bbs (2d array): of fastai formatted bbs
+            gt_cats (1d array): of categories of each object detected
+                ordering matches that of the bbs
+        """
         image_ids = self.get_image_ids()
         image_id = image_ids[idx]
         ann = self.get_annotations()[image_id]
