49b2737892a547ccf45f32356be42f81e7959a1e
aaronlelevier <aaron.lelevier@gmail.com>
2018-11-18 19:50:44 -0800

    update train.py to have separate functions for building the model and the training loop, so we can re-run the training loop. Also added in a validation set epoch loop for cross checking against the validation set and saving it's losses

--- train.py
+++ train.py
@@ -1,10 +1,12 @@
+import enum
 import time
 
+import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 
 from ssdmultibox.criterion import SSDLoss
-from ssdmultibox.datasets import TrainPascalDataset, device
+from ssdmultibox.datasets import PascalDataset, TrainPascalDataset, ValPascalDataset, device
 from ssdmultibox.models import SSDModel
 
 EPOCHS = 1
@@ -14,39 +16,72 @@ LR = 0.01
 CLIP = 0.5
 
 
-def main():
-    # data
-    dataset = TrainPascalDataset()
-    dataloader = DataLoader(dataset, batch_size=BATCH, num_workers=NUM_WORKERS)
+class Phase(enum.Enum):
+    TRAIN = 'TRAIN'
+    VAL = 'VAL'
 
-    # model
-    model = SSDModel()
-    model = model.to(device)
+
+if 'losses' not in locals(): losses = {Phase.TRAIN: [], Phase.VAL: []}
+
+
+def get_dataloaders():
+    train_dataset = TrainPascalDataset()
+    val_dataset = ValPascalDataset()
+    return {
+        Phase.TRAIN: DataLoader(
+            train_dataset, batch_size=BATCH, num_workers=NUM_WORKERS, shuffle=True),
+        Phase.VAL: DataLoader(
+            val_dataset, batch_size=BATCH, num_workers=NUM_WORKERS)
+    }
+
+
+def get_model():
+    return SSDModel().to(device)
+
+
+def train(model, dataloaders, epochs):
     criterion = SSDLoss()
     optimizer = optim.SGD(model.parameters(), lr=LR)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
     current_time = time.time()
 
-    for epoch in range(EPOCHS):
-        for i, (image_ids, ims, gt_bbs, gt_cats) in enumerate(dataloader):
-            # put data on device
-            ims, gt_bbs, gt_cats = dataset.to_device(ims, gt_bbs, gt_cats)
+    for epoch in range(epochs):
+        scheduler.step()
+        for phase in [Phase.TRAIN, Phase.VAL]:
+            dataloader = dataloaders[phase]
+            if phase == Phase.TRAIN:
+                model.train()
+            else:
+                model.eval()
 
-            # forward pass
-            preds = model(ims)
-            loss = criterion(preds, (gt_bbs, gt_cats))
+            for i, (image_ids, ims, gt_bbs, gt_cats) in enumerate(dataloader):
+                # put data on device
+                ims, gt_bbs, gt_cats = PascalDataset.to_device(ims, gt_bbs, gt_cats)
 
-            # backward pass
-            optimizer.zero_grad()
-            loss.backward()
-            nn.utils.clip_grad_norm_(model.parameters(), CLIP)
-            optimizer.step()
+                # zero out gradients
+                optimizer.zero_grad()
 
-            # stats
-            print(i, 'loss:', loss, 'time:', time.time() - current_time)
-            current_time = time.time()
+                # forward pass
+                with torch.set_grad_enabled(phase == Phase.TRAIN):
+                    preds = model(ims)
+                    loss = criterion(preds, (gt_bbs, gt_cats))
 
-            break
-        break
+                    # backward pass
+                    if phase == Phase.TRAIN:
+                        loss.backward()
+                        optimizer.step()
+
+                    # stats
+                    if i % 5 == 0:
+                        print(i, 'loss:', loss.item(), 'time:', time.time() - current_time)
+                        current_time = time.time()
+                        losses[phase].append(loss.item())
+
+
+def main():
+    model = get_model()
+    dataloaders = get_dataloaders()
+    train(model, dataloaders, epochs=1)
 
 
 if __name__ == '__main__':
