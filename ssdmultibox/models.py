6811533384099a8fc8993b985e3886d4aa97b34b
aaronlelevier <aaron.lelevier@gmail.com>
2018-12-08 09:39:26 -0800

    reorder models.py class top down
    clean up vgg16_bn and make_layers so cfg import from torchvision could be removed because this would cause confusion with this projects global model config also named 'cfg'

--- ssdmultibox/models.py
+++ ssdmultibox/models.py
@@ -3,62 +3,76 @@ import torch.nn.functional as F
 import torch.utils.model_zoo as model_zoo
 from torch import nn
 from torchvision.models import VGG
-from torchvision.models.vgg import cfg, model_urls
+from torchvision.models.vgg import model_urls
 
 from ssdmultibox.datasets import NUM_CLASSES
 
 
-class OutConv(nn.Module):
-    def __init__(self, nin):
-        super().__init__()
-        self.bbs_size = 4
-        self.oconv1 = nn.Conv2d(nin, self.bbs_size, 3, padding=1)
-        self.oconv2 = nn.Conv2d(nin, NUM_CLASSES, 3, padding=1)
-
-    def forward(self, x):
-        return [self.flatten_conv(self.oconv1(x), self.bbs_size),
-                self.flatten_conv(self.oconv2(x), NUM_CLASSES)]
-
-    def flatten_conv(self, x, size):
-        bs,nf,gx,gy = x.size()
-        x = x.permute(0,2,3,1).contiguous()
-        return x.view(bs,-1, size)
-
-
-class OutCustomHead(nn.Module):
-    """
-    Returns the final stacked predictions as a tuple (bbs, cats)
-    """
+class SSDModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.aspect_ratio_count = 6
-        self._setup_outconvs()
+        self.vgg_base = self._create_vgg_base()
+        self.blocks_model = BlocksCustomHead()
+        self.out_conv_head = OutCustomHead()
 
-    def _setup_outconvs(self):
-        block_names = ['block4', 'block7', 'block8', 'block9', 'block10', 'block11']
-        block_sizes = [512, 1024, 512, 256, 256, 256]
-        for i, name in enumerate(block_names):
-            for j, ar in enumerate(range(self.aspect_ratio_count)):
-                setattr(self, f'{block_names[i]}_{j}', OutConv(block_sizes[i]))
+    def forward(self, x):
+        x = self.vgg_base(x)
+        x = self.blocks_model(x)
+        return self.out_conv_head(x)
 
-    def forward(self, blocks):
-        all_bbs = None
-        all_cats = None
-        first_loop = True
-        for k,v in blocks.items():
-            for i in range(self.aspect_ratio_count):
-                # NOTE: maybe shouldn't be accessing the `_modules` private OrderedDict here ...
-                fm_ar_outconv = self._modules[f'{k}_{i}']
-                bbs, cats = fm_ar_outconv(v)
-                # just need to check bbs or cats here
-                if first_loop:
-                    all_bbs = bbs
-                    all_cats = cats
-                    first_loop = False
-                else:
-                    all_bbs = torch.cat((all_bbs, bbs), dim=1)
-                    all_cats = torch.cat((all_cats, cats), dim=1)
-        return all_bbs, all_cats
+    def _create_vgg_base(self):
+        vgg_base = vgg16_bn(pretrained=True)
+        for layer in vgg_base.parameters():
+            layer.requires_grad = False
+        return vgg_base
+
+def vgg16_bn(pretrained=False, **kwargs):
+    """VGG 16-layer model (configuration "D") with batch normalization
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = VGGBase(make_layers(), **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['vgg16_bn']))
+    return model
+
+
+class VGGBase(VGG):
+    def __init__(self, features, num_classes=1000, init_weights=True):
+        # features are really layers
+        super().__init__(features, num_classes, init_weights)
+
+    def forward(self, x):
+        feat_layers = ['block4', 'block7', 'block8', 'block9', 'block10', 'block11']
+        blocks = {b:None for b in feat_layers}
+
+        for i, feature in enumerate(self.features):
+            x = feature(x)
+            if i == 32:
+                blocks['block4'] = x
+            # don't call the final max_pool layer b/c we want an output shape of (512, 19, 19)
+            if i == 42:
+                break
+        return x, blocks
+
+
+def make_layers():
+    layer_config = [
+        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+    layers = []
+    in_channels = 3
+    for v in layer_config:
+        if v == 'M':
+            # ceil_mode=False in the normal function
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
 
 
 class BlocksCustomHead(nn.Module):
@@ -102,71 +116,54 @@ class BlocksCustomHead(nn.Module):
         return blocks
 
 
-class SSDModel(nn.Module):
-    def __init__(self):
-        # features are really layers
-        super().__init__()
-        self.vgg_base = self._create_vgg_base()
-        self.blocks_model = BlocksCustomHead()
-        self.out_conv_head = OutCustomHead()
-
-    def forward(self, x):
-        x = self.vgg_base(x)
-        x = self.blocks_model(x)
-        return self.out_conv_head(x)
-
-    def _create_vgg_base(self):
-        vgg_base = vgg16_bn(pretrained=True)
-        for layer in vgg_base.parameters():
-            layer.requires_grad = False
-        return vgg_base
-
-
-def make_layers(cfg, batch_norm=False):
-    layers = []
-    in_channels = 3
-    for v in cfg:
-        if v == 'M':
-            # ceil_mode=False in the normal function
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
-        else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
-            else:
-                layers += [conv2d, nn.ReLU(inplace=True)]
-            in_channels = v
-    return nn.Sequential(*layers)
-
-
-class VGGBase(VGG):
-    def __init__(self, features, num_classes=1000, init_weights=True):
-        # features are really layers
-        super().__init__(features, num_classes, init_weights)
-
-    def forward(self, x):
-        feat_layers=['block4', 'block7', 'block8', 'block9', 'block10', 'block11']
-        blocks = {b:None for b in feat_layers}
-
-        for i, feature in enumerate(self.features):
-            x = feature(x)
-            if i == 32:
-                blocks['block4'] = x
-            # don't call the final max_pool layer b/c we want an output shape of (512, 19, 19)
-            if i == 42:
-                break
-        return x, blocks
-
-
-def vgg16_bn(pretrained=False, **kwargs):
-    """VGG 16-layer model (configuration "D") with batch normalization
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
+class OutCustomHead(nn.Module):
     """
-    if pretrained:
-        kwargs['init_weights'] = False
-    model = VGGBase(make_layers(cfg['D'], batch_norm=True), **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['vgg16_bn']))
-    return model
+    Returns the final stacked predictions as a tuple (bbs, cats)
+    """
+    def __init__(self):
+        super().__init__()
+        self.aspect_ratio_count = 6
+        self._setup_outconvs()
+
+    def _setup_outconvs(self):
+        block_names = ['block4', 'block7', 'block8', 'block9', 'block10', 'block11']
+        block_sizes = [512, 1024, 512, 256, 256, 256]
+        for i, name in enumerate(block_names):
+            for j, ar in enumerate(range(self.aspect_ratio_count)):
+                setattr(self, f'{block_names[i]}_{j}', OutConv(block_sizes[i]))
+
+    def forward(self, blocks):
+        all_bbs = None
+        all_cats = None
+        first_loop = True
+        for k,v in blocks.items():
+            for i in range(self.aspect_ratio_count):
+                # NOTE: maybe shouldn't be accessing the `_modules` private OrderedDict here ...
+                fm_ar_outconv = self._modules[f'{k}_{i}']
+                bbs, cats = fm_ar_outconv(v)
+                # just need to check bbs or cats here
+                if first_loop:
+                    all_bbs = bbs
+                    all_cats = cats
+                    first_loop = False
+                else:
+                    all_bbs = torch.cat((all_bbs, bbs), dim=1)
+                    all_cats = torch.cat((all_cats, cats), dim=1)
+        return all_bbs, all_cats
+
+
+class OutConv(nn.Module):
+    def __init__(self, nin):
+        super().__init__()
+        self.bbs_size = 4
+        self.oconv1 = nn.Conv2d(nin, self.bbs_size, 3, padding=1)
+        self.oconv2 = nn.Conv2d(nin, NUM_CLASSES, 3, padding=1)
+
+    def forward(self, x):
+        return [self.flatten_conv(self.oconv1(x), self.bbs_size),
+                self.flatten_conv(self.oconv2(x), NUM_CLASSES)]
+
+    def flatten_conv(self, x, size):
+        bs,nf,gx,gy = x.size()
+        x = x.permute(0,2,3,1).contiguous()
+        return x.view(bs,-1, size)
